f89c867f1c23f195a5914311f0e9b732e07cd5d0
Almalerik <almalerik@gmail.com>
2015-12-12 07:02:44 +0100

    ...

---
+++ assets/admin/js/customizer-init.js
@@ -0,0 +1,55 @@
+jQuery(document).ready(function() {
+
+	// Move title tagline in header panel
+	wp.customize.section('title_tagline').panel('loungeact_header');
+	wp.customize.section('title_tagline').priority('20');
+
+	// Header widget
+	wp.customize.section('sidebar-widgets-homepage-features').panel('loungeact_homepage_features');
+	wp.customize.section('sidebar-widgets-homepage-features').priority('20');
+
+	// //jQuery(wp.customize.control('alcor_container_class_fixed_max_width').container).css("display","none");
+
+	// Show or hide wrapper container max width
+	if (wp.customize('loungeact[container_class]').get() === 'container-fluid') {
+		wp.customize.control('loungeact_container_max_width').deactivate();
+	} else {
+		wp.customize.control('loungeact_container_max_width').activate();
+	}
+	jQuery('#customize-control-loungeact_container_class input').on('change', function() {
+		if (wp.customize('loungeact[container_class]').get() === 'container-fluid') {
+			wp.customize.control('loungeact_container_max_width').deactivate();
+		} else {
+			wp.customize.control('loungeact_container_max_width').activate();
+		}
+	});
+	
+	// Show or hide slider height
+	if (wp.customize('loungeact[slider_fullscreen]').get() === true) {
+		wp.customize.control('loungeact_slider_height').deactivate();
+	} else {
+		wp.customize.control('loungeact_slider_height').activate();
+	}
+	jQuery('#customize-control-loungeact_slider_fullscreen input').on('change', function() {
+		if (this.checked) {
+			wp.customize.control('loungeact_slider_height').deactivate();
+		} else {
+			wp.customize.control('loungeact_slider_height').activate();
+		}
+	});
+	
+	jQuery('body').on('click', '.lougeact-goto-swh-features', function(){
+	    wp.customize.section('sidebar-widgets-homepage-features').focus();
+	});
+	
+	
+	jQuery('#customize-control-loungeact_header_fixed_top select').on('change', function() {
+		if (jQuery(this).val == "") {
+			wp.customize.control('header_margin_bottom').deactivate();
+		} else {
+			wp.customize.control('header_margin_bottom').activate();
+		}
+	});
+	
+	
+});
---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Lounge Act Theme Customizer.
+ *
+ * @package Lounge_Act
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function lounge_act_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'lounge_act_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function lounge_act_customize_preview_js() {
+	wp_enqueue_script( 'lounge_act_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
+}
+add_action( 'customize_preview_init', 'lounge_act_customize_preview_js' );
