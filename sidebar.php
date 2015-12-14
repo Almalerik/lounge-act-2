a41111316f13026823ccd6fcd9a1a7e92e1d2eb7
Almalerik <almalerik@gmail.com>
2015-12-14 06:36:12 +0100

    ...

--- sidebar.php
+++ sidebar.php
@@ -11,7 +11,11 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	return;
 }
 ?>
-
+<!-- 
 <aside id="secondary" class="widget-area" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </aside><!-- #secondary -->
+
+<div id="secondary" class="widget-area" role="complementary">
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+</div><!-- #secondary -->
